6d4562dc2ff4889bca9e7acfaa6ce5b9d9fa36cf
manishreddyabsyz <manish.naini@absyz.com>
2026-01-22 17:13:44 +0530

    Initial commit: Salesforce MCP POC

---
+++ server.py
@@ -0,0 +1,14 @@
+from mcp.server.fastmcp import FastMCP
+from simple_salesforce import Salesforce
+from dotenv import load_dotenv
+import os
+load_dotenv()
+mcp=FastMCP()
+sf = Salesforce(
+    username=os.getenv("SF_USERNAME"),
+    password=os.getenv("SF_PASSWORD"),
+    security_token=os.getenv("SF_TOKEN"),
+    domain=os.getenv("SF_DOMAIN", "login")
+)
+if __name__ == "__main__":
+    mcp.run()
